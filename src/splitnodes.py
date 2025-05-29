b20ee2c577410ccd0067bd311fafa1cff76ba2fa
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-05-28 22:17:55 -0700

    Replaced image splitting function, removing issue where ! in the text would cause a failure.

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,4 +1,5 @@
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from extract_images_links import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
@@ -9,31 +10,56 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new
 
 def split_nodes_image(old_nodes):
-    text_node_list = []
-    image = False
-    remainder = old_nodes[0]
-    index = remainder.find("[")
-    while len(remainder) > 0:
-        if remainder[0] == "!":
-            image = True
-        else:
-            image = False
-
-        if image:
-            text = remainder[2:remainder.find("]")]
-            url = remainder[remainder.find("(")+1:remainder.find(")")]
-            text_node_list.append(text, TextType.IMAGE, url)
-            index = remainder.find(")")+1
-            remainder = remainder[index:]
-            if remainder.find("[") == -1:
-                text_node_list.append(remainder, TextType.TEXT)
-                remainder = ""
-        else:
-            text_node_list.append(remainder[:index-1], TextType.TEXT)
-            index = remainder.find("[")-1
+    new = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new.append(TextNode(sections[0], TextType.TEXT))
+            new.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new.append(TextNode(original_text, TextType.TEXT))
+    return new
             
         
             
 
 def split_nodes_link(old_nodes):
-    pass
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = text_node_to_html_node(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
